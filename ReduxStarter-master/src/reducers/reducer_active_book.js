9c694217a92ae82a8116b42b46d6e620bdd3f80d
darshan221b <darshanu53@gmail.com>
2018-07-12 15:15:30 +0530

    redux thur

---
+++ ReduxStarter-master/src/reducers/reducer_active_book.js
@@ -0,0 +1,13 @@
+//state argument is not application state, only the state
+// this reducer is responsible for
+
+export default function(state = null, action) {  // if arguments is undefined it sets it to null
+    
+    switch(action.type) {
+        case 'BOOK_SELECTED':
+            return action.payload;
+    }
+    
+    return state;
+
+}
--- ReduxStarter-master/src/reducers/reducer_books.js
+++ ReduxStarter-master/src/reducers/reducer_books.js
@@ -5,4 +5,4 @@ export default function() {
         { title : 'CC'},
         { title : 'DD'}
     ]
-}
+} 
