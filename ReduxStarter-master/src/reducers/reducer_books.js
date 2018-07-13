5f623a469c5f825271e29f5b6545603689c62b4f
darshan221b <darshanu53@gmail.com>
2018-07-13 12:15:22 +0530

    final day

--- ReduxStarter-master/src/reducers/reducer_books.js
+++ ReduxStarter-master/src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title : 'AA'},
-        { title : 'BB'},
-        { title : 'CC'},
-        { title : 'DD'}
+        { title : 'AA', pages: 100},
+        { title : 'BB', pages: 75},
+        { title : 'CC', pages: 50},
+        { title : 'DD', pages: 25}
     ]
 } 
